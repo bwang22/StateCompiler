02be8e37454f26e5e3b5bfa567d9f286e8e8a5a6
bwang22 <ttwanksta@gmail.com>
2018-02-10 23:40:06 -0500

    xcodeproj

---
+++ StateCompiler/StateCompiler/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  StateCompiler
+//
+//  Created by Ben Wang on 2/10/18.
+//  Copyright © 2018 Ben Wang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World! Ben\n");
+    return 0;
+}
